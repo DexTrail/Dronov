cb81ff5adcf2935dd45c5012e1e69739fc0f0cb1
Dmitry Ivakhnenko <dmitry.ivakhnenko@gmail.com>
2019-11-06 13:13:19 +0300

    Add first files

---
+++ bboard/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Bb
+from .models import Rubric
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {
+        'bbs': bbs,
+        'rubrics': rubrics
+    }
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {
+        'bbs': bbs,
+        'rubrics': rubrics,
+        'current_rubric': current_rubric
+    }
+    return render(request, 'bboard/by_rubric.html', context)
+
+
+def test(request):
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
